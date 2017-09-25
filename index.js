19038e8d598ffe26807a9bbdd2ed00509539d934
Davin Dillon <davindillon@gmail.com>
2017-09-25 17:52:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ function createRock(x) {
      function step() {
        rock.style.top = `${top += 2}px`
 
-       if (top < 360) {
+       if (top < 400) {
          window.requestAnimationFrame(step)
        }
      }
